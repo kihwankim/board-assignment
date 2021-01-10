12debae0d93d66f9cabaf145d5cddb81c6fc73a5
NHN <nhn@AL01539415.local>
2021-01-10 21:42:25 +0900

    feat(board-view): 게시판 데이터 조회 프론트 엔드 작성

--- app/Views/board_view.php
+++ app/Views/board_view.php
@@ -2,22 +2,40 @@
 
 <?= $this->section('content') ?>
 <div id="app">
+    <template>
     <v-container>
-        <v-card style="width: 30%, height=200">
+        <v-card 
+            elevation="2" 
+            outlined 
+            style="width: 30%, height=200">
             <v-toolbar dark>
                 <v-toolbar-title><?= $board['title'] ?></v-toolbar-title>
-                <?= $board['create_at'] ?>
+                <v-subheader><?= $board['create_at'] ?></v-subheader>
             </v-toolbar>
-            <v-card-subtitle><?= $board['writer'] ?></v-card-subtitle>
-            <?= $board['description'] ?>
+            <v-card-subtitle align="right">
+                writer : <?= $board['writer'] ?>
+            </v-card-subtitle>
+            <v-card-text>
+                <?= $board['description'] ?>
+            </v-card-text>
         </v-card>
-        <v-btn 
-            color="red" 
-            elevation="24"
-            @click="back">
-            Back
-        </v-btn>
+        <div align="right">
+            <v-btn 
+            large
+            elevation="2"
+            >
+                delete
+            </v-btn>
+            <v-btn 
+            large
+            color="red"
+            elevation="2"
+            >
+                edit
+            </v-btn>
+      </div>
     </v-container>
+    </template>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
@@ -26,6 +44,9 @@
     new Vue({ 
       el: '#app',
       vuetify: new Vuetify(),
+      data: {
+          person: 'hello'
+      }
     })
 </script>
 <?= $this->endSection() ?>
