5887bbce6e312631089fcd20e25b9b95d81fe922
NHN <nhn@AL01539415.local>
2021-01-10 15:59:01 +0900

    feat(route): uri 설정

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->group('home', function($routes) {
+	$routes->get('board/(:any)', 'Home::getBoardDataById/$1');
+});
 
 /**
  * --------------------------------------------------------------------
