89720f0c64ad63dbb26dddd90605c97638bf492c
NHN <nhn@AL01539415.local>
2021-01-10 20:23:45 +0900

    feat(main): 메인화면 디자인 및 구조 작성

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -4,13 +4,14 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+  <link href="../assets/board.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
   <meta charset="utf-8"/>
   <title><?= (isset($meta_title) ? $meta_title : 'board title') ?></title>
 </head>
 <body>
-    <div>
-        <?= $this->renderSection('content') ?>
-    </div>
+    <?= $this->renderSection('content') ?>
 </body>
 </html>
